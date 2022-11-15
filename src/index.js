6fe82e39b282124abd3638cf14dd3b4d7078a51e
Derrick <djmulinde@gmail.com>
2022-11-15 14:12:02 +0300

    Add store to the app using provider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./components/missions/redux/store/Store";
+import Missions from "./components/missions/Missions";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+      <Missions />
+    </Provider>
   </React.StrictMode>
 );
