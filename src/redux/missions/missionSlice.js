80a29e65ccdbde144b5690fab032ae7c471ebd59
Derrick <djmulinde@gmail.com>
2022-11-15 17:23:04 +0300

    Fetch missions and store data in redux store

--- src/redux/missions/missionSlice.js
+++ src/redux/missions/missionSlice.js
@@ -1,24 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { fetchMissions } from './actions/missionActions';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const FETCH_MISSIONS = 'redux/missions/FETCH_MISSIONS';
+const url = 'https://api.spacexdata.com/v3/missions';
 
 const initialState = {
   missions: {},
   isLoading: true,
 }
 
+export const FetchMissions = createAsyncThunk(
+  FETCH_MISSIONS,
+  async (thunkAPI) => {
+    try {
+      const missions = await axios.get(url);
+      return missions.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
 const MissionsSlice = createSlice({
   name: 'missions',
   initialState,
   reducers: {},
   extraReducers: {
-    [fetchMissions.pending]: (state) => {
+    [FetchMissions.pending]: (state) => {
       state.isLoading = true;
     },
-    [fetchMissions.fulfilled]: (state, action) => {
+    [FetchMissions.fulfilled]: (state, action) => {
       state.isLoading = false;
-      state.missions = action.payload;
+      action.payload.map((mission) => {
+        return state.missions = {
+          id: mission.mission_id,
+          mission_name: mission.mission_name,
+          description: mission.description,
+        }
+      })
     },
-    [fetchMissions.rejected]: (state) => {
+    [FetchMissions.rejected]: (state) => {
       state.isLoading = false;
     }
   }
