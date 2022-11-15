6a800ebcd9d8405f42c3a7c0a13b5e3509dec784
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-11-15 16:14:36 +0200

    Fetch data from rockets endpoint

--- src/redux/rockets/rocketSlice.js
+++ src/redux/rockets/rocketSlice.js
@@ -1,18 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
 // Slice
 
 const slice = createSlice({
     name: 'rockets',
     initialState: {
-        rockets: []
+        rockets: [],
     },
     reducers: {
-        getRockets: (state, action) => {
+        rocketsSuccess: (state, action) => {
             state.rockets = action.payload;
+            state.isLoading = false;
         },
     },
 });
+
+// Actions
+
+const apiURL = 'https://api.spacexdata.com/v3/rockets';
+
+const { rocketsSuccess } = slice.actions
+
+export const fetchrockets = () => async dispatch => {
+    try {
+        await apiURL.get('/rockets')
+            .then((response) => dispatch(rocketsSuccess(response.data)))
+    }
+    catch (e) {
+        return console.error(e.message);
+    }
+}
 
 export default slice.reducer
 
