ebf1d68737e6feb7a3ccde7a7074a0751acd4347
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-11-16 14:51:08 +0200

    Update Rockets.js

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchRockets } from "../../redux/rockets/rocketSlice";
 
 const Rockets = () => {
+  const rockets = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+  
   return (
     <div>Rockets</div>
-  )
-}
+
+    );
+};
 
 export default Rockets;
