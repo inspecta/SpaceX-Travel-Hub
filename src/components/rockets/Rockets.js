826a1ec6994d053e9d1e35d297a7795c799a6630
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-11-16 19:29:18 +0200

    Display Rockets

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -3,17 +3,46 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchRockets } from "../../redux/rockets/rocketSlice";
 
 const Rockets = () => {
-  const rockets = useSelector((state) => state.rockets);
+  const { rockets, status } = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchRockets());
-  }, [dispatch]);
+    if (status === null) {
+      dispatch(fetchRockets());
+    }
+  }, [status, dispatch]);
   
-  return (
-    <div>Rockets</div>
+  let content;
+  if (status === 'pending') {
+    content = <p>Loading...</p>;
+  } else if (status === 'rejected') {
+    content = <p>An error occured</p>;
+  } else if (status === 'success') {
+    content = <p></p>;
+  }
 
-    );
+  return (
+    <div>
+      {content}
+      <div className="rocketList">
+        {rockets.map((rocket) => (
+          <div key={rocket.id} className="rocketCard">
+            <img src={rocket.flickr_images[0]} alt="" />
+            <div className="rocketContent">
+              <h3 className="rocketName">{rocket.rocket_name}</h3>
+              <div className="rocketDesc">
+                <p>
+                  <span className="status">status</span>
+                  <span>{rocket.description}</span>
+                </p>
+              </div>
+              <button type="submit">Reserve Rocket</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Rockets;
