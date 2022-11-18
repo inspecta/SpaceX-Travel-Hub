a23808e3870faf48d6dcb21719fee0df8c40e950
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-11-18 23:03:10 +0200

    Update testing

--- src/tests/Rockets.test.js
+++ src/tests/Rockets.test.js
@@ -1,5 +1,4 @@
 import '@testing-library/jest-dom';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 import store from '../redux/configureStore';
@@ -8,11 +7,9 @@ import Rockets from '../components/rockets/Rockets';
 describe('Rocket page testing', () => {
   test('Does Rocket match the snapshot', () => {
     const rocket = render(
-      <BrowserRouter>
-        <Provider store={store}>
-          <Rockets />
-        </Provider>
-      </BrowserRouter>,
+    <Provider store={store}>
+      <Rockets />
+    </Provider>,
     );
     expect(rocket).toMatchSnapshot();
   });
